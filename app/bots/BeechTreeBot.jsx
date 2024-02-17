be80656fd234f4723700d169a206da3f356e9cf0
Adam Boettcher <thebeech@yahoo.com>
2024-02-17 15:53:37 -0800

    update flowise id

--- app/bots/BeechTreeBot.jsx
+++ app/bots/BeechTreeBot.jsx
@@ -5,10 +5,9 @@ import { BubbleChat } from 'flowise-embed-react'
 
 const BeechTreeBot = () => {
     return (
-        <BubbleChat chatflowid="1b2d5f4f-cae8-45c6-8bb1-4aba477e4763" apiHost="https://flowise-test-1-n7v0.onrender.com" />
+        <BubbleChat chatflowid="c2839b4f-76d1-4608-b774-47da781395cb" apiHost="https://flowise-test-1-n7v0.onrender.com" />
     );
 };
 
 
 export default BeechTreeBot
-
--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,12 @@ import SocialProof from '../src/components/widgets/SocialProof';
 import Features3 from '~/components/widgets/Features3';
 import Features4 from '~/components/widgets/Features4';
 import Content from '~/components/widgets/Content';
-import Steps from '~/components/widgets/Steps';
-import Testimonial from '~/components/widgets/Testimonial';
-import FAQs2 from '~/components/widgets/FAQs2';
-import Pricing from '~/components/widgets/Pricing';
-import Team from '~/components/widgets/Team';
-import CallToAction2 from '~/components/widgets/CallToAction2';
+// import Steps from '~/components/widgets/Steps';
+// import Testimonial from '~/components/widgets/Testimonial';
+// import FAQs2 from '~/components/widgets/FAQs2';
+// import Pricing from '~/components/widgets/Pricing';
+// import Team from '~/components/widgets/Team';
+// import CallToAction2 from '~/components/widgets/CallToAction2';
 
 import {
   callToActionData,
