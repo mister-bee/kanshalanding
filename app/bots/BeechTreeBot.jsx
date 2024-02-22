eec9ed0e687cbe2623f05c0ee6fd39f59337158a
Adam Boettcher <thebeech@yahoo.com>
2024-02-21 18:28:24 -0800

    new chatbot id

--- app/bots/BeechTreeBot.jsx
+++ app/bots/BeechTreeBot.jsx
@@ -5,7 +5,9 @@ import { BubbleChat } from 'flowise-embed-react'
 
 const BeechTreeBot = () => {
     return (
-        <BubbleChat chatflowid="c2839b4f-76d1-4608-b774-47da781395cb" apiHost="https://flowise-test-1-n7v0.onrender.com" />
+        // <BubbleChat chatflowid="c2839b4f-76d1-4608-b774-47da781395cb" apiHost="https://flowise-test-1-n7v0.onrender.com" />
+        <BubbleChat chatflowid="64b1fe4b-3fee-4edd-aff5-108e3f3b42c9" apiHost="https://flowiserotary.onrender.com" />
+
     );
 };
 
